8363339f037b7c65c76455f2d0cad0a321d824e6
Volodymyr Kovalchuk <volodymyr.kovalchuk@atos.net>
2019-02-26 15:15:32 +0100

    Updates

---
+++ precache-manifest.1d88cf8b43ea090167cedcea8ddf87c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/optimizeMyEvents/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "977eb3025b505d854357",
+    "url": "/optimizeMyEvents/static/js/runtime~main.977eb302.js"
+  },
+  {
+    "revision": "a4d16fa1927172459040",
+    "url": "/optimizeMyEvents/static/js/main.a4d16fa1.chunk.js"
+  },
+  {
+    "revision": "5bf9b2cda7c63b962686",
+    "url": "/optimizeMyEvents/static/js/2.5bf9b2cd.chunk.js"
+  },
+  {
+    "revision": "a4d16fa1927172459040",
+    "url": "/optimizeMyEvents/static/css/main.0bb5f322.chunk.css"
+  },
+  {
+    "revision": "f65c618ec3748b0a20b030d128104bd7",
+    "url": "/optimizeMyEvents/index.html"
+  }
+];
